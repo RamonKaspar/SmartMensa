04305142f284118da1efd1dcab55e756cfdcf3ca
Lukas Mast <lukasmast07@gmail.com>
2023-11-29 01:44:32 +0100

    Add home component

---
+++ src/client/home/Home.tsx
@@ -0,0 +1,16 @@
+import Layout from "../Layout";
+import Header from "../Header";
+import HomeBody from "./HomeBody";
+
+function Login() {
+  return (
+    <>
+      <Layout>
+        <Header />
+        <HomeBody />
+      </Layout>
+    </>
+  );
+}
+
+export default Login;
