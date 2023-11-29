5302f602ac74e5b115bac9cc335489dff464fd82
Lukas Mast <lukasmast07@gmail.com>
2023-11-29 01:42:01 +0100

    Add routes which are used by the BrowserRouter

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,11 +1,19 @@
 import "./App.css";
-import Layout from "./Layout";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "./login/Login";
+import Home from "./home/Home";
+import Mensa from "./mensa/Mensa";
 
 function App() {
   return (
-      <Layout>
-      TODO: implement me!
-      </Layout>
+    <>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/:mensa" element={<Mensa />} />
+      </Routes>
+    </>
   );
 }
 
