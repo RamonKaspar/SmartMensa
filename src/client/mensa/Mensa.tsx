456110e74012af1988600a2c4e30af645c6d9d2d
Lukas Mast <lukasmast07@gmail.com>
2023-11-29 01:46:41 +0100

    Add mensa component

---
+++ src/client/mensa/Mensa.tsx
@@ -0,0 +1,18 @@
+import Layout from "../Layout";
+import Header from "../Header";
+import MensaBody from "./MensaBody";
+import Footer from "../Footer";
+
+function Mensa() {
+  return (
+    <>
+      <Layout>
+        <Header />
+        <MensaBody />
+        <Footer />
+      </Layout>
+    </>
+  );
+}
+
+export default Mensa;
