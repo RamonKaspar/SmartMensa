d412b8e77a4254a18c43e2930229be263dd0ba13
Lukas Mast <lumast@student.ethz.ch>
2023-11-30 16:58:10 +0100

    Add unique key to each list and restructure

--- src/client/login/LoginBody.tsx
+++ src/client/login/LoginBody.tsx
@@ -1,7 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import "./LoginBody.css";
 import { useState } from "react";
-import LogoAsImage from './logo.png';
 
 function LoginBody() {
   const navigate = useNavigate();
@@ -10,23 +9,38 @@ function LoginBody() {
   };
 
   // TAKE USERNAME FROM HERE WHEN WE HAVE USER MANAGEMENT
-  const [username, setUsername] = useState('');
+  const [username, setUsername] = useState("");
   const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setUsername(event.target.value);
   };
   // TAKE PASSWORD FROM HERE WHEN WE HAVE USER MANAGEMENT
-  const [password, setPassword] = useState('');
+  const [password, setPassword] = useState("");
   const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setPassword(event.target.value);
   };
 
   return (
     <main className="login-body-container">
-        <input className="username-input" type="text" value={username} onChange={handleUsernameChange} placeholder="Username" />
-        <input className="password-input" type="password" value={password} onChange={handlePasswordChange} placeholder="Password"/>
-        <button onClick={handleClick}>Submit</button>
-        <div className="register">Are you the first time visiting this page? Please register <a href="https://ethz.ch/de.html">here</a></div>
-        <img src={LogoAsImage} alt="SmartMensa" className="logo"/>
+      <input
+        className="username-input"
+        type="text"
+        value={username}
+        onChange={handleUsernameChange}
+        placeholder="Username"
+      />
+      <input
+        className="password-input"
+        type="password"
+        value={password}
+        onChange={handlePasswordChange}
+        placeholder="Password"
+      />
+      <button onClick={handleClick}>Submit</button>
+      <div className="register">
+        Are you the first time visiting this page? Please register{" "}
+        <a href="https://ethz.ch/de.html">here</a>
+      </div>
+      <img src="./logo.png" alt="SmartMensa" className="logo" />
     </main>
   );
 }
