1f113b12ace1672ad77f9b88db88c5c78436cfec
Lukas Mast <lumast@student.ethz.ch>
2023-12-06 00:51:43 +0100

    Use node-cron to schedule python scripts

--- src/server/main.ts
+++ src/server/main.ts
@@ -2,15 +2,66 @@ import express from "express";
 import ViteExpress from "vite-express";
 import path from "path";
 import fs from "fs/promises";
+import { spawn } from "child_process";
+import cron from "node-cron";
 
 const app = express();
+
+const pythonScriptPathUZH = path.join(__dirname, "menu_scraper_uhz.py");
+const pythonScriptPathETH = path.join(__dirname, "menu_scraper_eth.py");
+
+// Schedule the execution of the menu scraper scripts (ETH and UZH) every Monday at 00:05
+cron.schedule(
+  // "5 0 * * 1", // Run every Monday at 00:05
+  "*/5 * * * *", // For testing purposes, run every 5 minutes
+  () => {
+    console.log("Fetching new UZH menus...");
+
+    // Spawn a new python process to run the menu scraper script for UZH
+    const pythonProcess_uzh = spawn("python", [pythonScriptPathUZH]);
+
+    pythonProcess_uzh.stdout.on("data", (data) => {
+      const output = data.toString().trim();
+      console.log(output);
+    });
+
+    pythonProcess_uzh.stderr.on("data", (data) => {
+      console.error(`Python script stderr: ${data}`);
+    });
+
+    pythonProcess_uzh.on("close", (code) => {
+      console.log(`Python script process exited with code ${code}`);
+    });
+
+    console.log("Fetching new ETH menus...");
+    // Spawn a new python process to run the menu scraper script for ETH
+    const pythonProcess_eth = spawn("python", [pythonScriptPathETH]);
+
+    pythonProcess_eth.stdout.on("data", (data) => {
+      const output = data.toString().trim();
+      console.log(output);
+    });
+
+    pythonProcess_eth.stderr.on("data", (data) => {
+      console.error(`Python script stderr: ${data}`);
+    });
+
+    pythonProcess_eth.on("close", (code) => {
+      console.log(`Python script process exited with code ${code}`);
+    });
+  },
+  {
+    scheduled: true,
+    timezone: "Europe/Zurich",
+  }
+);
 
 // Route to serve files based on facilityID
 app.get("/menus/:facilityID", async function (req, res) {
   const facilityID = req.params.facilityID;
   const filePath = path.join(
     __dirname,
-    `/menus-as-json/menus-facility-${facilityID}.json`
+    `../../menus-as-json/menus-facility-${facilityID}.json`
   );
 
   try {
