0eb3780dfac53e5cd4740bb8fbf81bc98df21dba
Lukas Mast <lumast@student.ethz.ch>
2023-12-07 00:49:10 +0100

    Fix merge conflicts manually

--- src/server/main.ts
+++ src/server/main.ts
@@ -2,10 +2,10 @@ import express from "express";
 import ViteExpress from "vite-express";
 import path from "path";
 import fs from "fs/promises";
+// import apiRoutes from "./routes/api";
+import session from "express-session";
 // import { spawn } from "child_process";
 // import cron from "node-cron";
-import apiRoutes from "./routes/api";
-import session from "express-session";
 
 const app = express();
 
@@ -21,15 +21,15 @@ app.use(
   })
 );
 
-app.use("/api", apiRoutes);
+// app.use("/api", apiRoutes);
 
-app.get("/api/current-user", (req, res) => {
-  if (req.session && req.session.userId) {
-    res.json({ userId: req.session.userId });
-  } else {
-    res.status(401).json({ message: "No user logged in" });
-  }
-});
+// app.get("/api/current-user", (req, res) => {
+//   if (req.session && req.session.userId) {
+//     res.json({ userId: req.session.userId });
+//   } else {
+//     res.status(401).json({ message: "No user logged in" });
+//   }
+// });
 
 // const pythonScriptPathUZH = path.join(__dirname, "menu_scraper_uhz.py");
 // const pythonScriptPathETH = path.join(__dirname, "menu_scraper_eth.py");
@@ -114,11 +114,6 @@ app.get("/menus/:facilityID", async function (req, res) {
     console.error(error); // Log the error for debugging
     res.status(404).json({ error: "File not found" });
   }
-});
-
-// example route which returns a message
-app.get("/hello", async function (_req, res) {
-  res.status(200).json({ message: "Hello World!" });
 });
 
 // Do not change below this line
