6dbc383764605b1cbc143f0fdc014065ec3c27e3
Lukas Mast <lumast@student.ethz.ch>
2023-12-07 00:58:56 +0100

    Fix merge conflicts manually

--- src/server/main.ts
+++ src/server/main.ts
@@ -2,10 +2,34 @@ import express from "express";
 import ViteExpress from "vite-express";
 import path from "path";
 import fs from "fs/promises";
+// import apiRoutes from "./routes/api";
+import session from "express-session";
 // import { spawn } from "child_process";
 // import cron from "node-cron";
 
 const app = express();
+
+app.use(express.json());
+
+// Configure session middleware
+app.use(
+  session({
+    secret: "your-secret-key", // Replace with a secure key
+    resave: false,
+    saveUninitialized: true,
+    cookie: { httpOnly: true, secure: false }, // Set secure to true if using HTTPS
+  })
+);
+
+// app.use("/api", apiRoutes);
+
+// app.get("/api/current-user", (req, res) => {
+//   if (req.session && req.session.userId) {
+//     res.json({ userId: req.session.userId });
+//   } else {
+//     res.status(401).json({ message: "No user logged in" });
+//   }
+// });
 
 // const pythonScriptPathUZH = path.join(__dirname, "menu_scraper_uhz.py");
 // const pythonScriptPathETH = path.join(__dirname, "menu_scraper_eth.py");
