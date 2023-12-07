98ecbc9dc8c067bfef1aaac62fa6f31d3bac90be
Lukas Mast <lumast@student.ethz.ch>
2023-12-07 12:53:52 +0100

    Fix routing bug and validate routes using backend

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -4,8 +4,38 @@ import Login from "./login/Login";
 import Home from "./home/Home";
 import Mensa from "./mensa/Mensa";
 import Register from "./register/Register";
+import { useState, useEffect } from "react";
+
+async function fetchMensaStaticInfos() {
+  try {
+    const response = await fetch(`/mensa-info`);
+    if (!response.ok) {
+      throw new Error("Failed to fetch mensa static infos");
+    }
+    const mensaInfo = await response.json();
+    return mensaInfo;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 function App() {
+  const [mensaRoutes, setMensaRoutes] = useState<any>([]);
+
+  useEffect(() => {
+    async function fetchMeals() {
+      try {
+        const mensaInfos = await fetchMensaStaticInfos();
+        const mensaArray = mensaInfos.mensas;
+        const namesArray: string[] = mensaArray.map((mensa: any) => mensa.name);
+        setMensaRoutes(namesArray);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchMeals();
+  }, []);
+
   return (
     <>
       <Routes>
@@ -13,7 +43,10 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/home" element={<Home />} />
-        <Route path="/:mensa" element={<Mensa />} />
+        {mensaRoutes.map((mensa: string) => (
+          <Route key={mensa} path={`/${mensa}`} element={<Mensa />} />
+        ))}
+        {/* <Route path="*" element={<Navigate to="/login" />} /> */}
       </Routes>
     </>
   );
