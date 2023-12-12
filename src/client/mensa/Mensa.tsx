630552a1f88cd237558f54c241ab4b20c027a751
Ramon Kaspar <kasparr@stundent.ethz.ch>
2023-12-12 22:43:51 +0100

    Settings are clickable in mensa view

--- src/client/mensa/Mensa.tsx
+++ src/client/mensa/Mensa.tsx
@@ -1,13 +1,36 @@
 import Layout from "../Layout";
 import Header from "../Header";
 import MensaBody from "./MensaBody";
+import Settings from "../Settings";
 
-function Mensa() {
+function Mensa({
+  showFilter,
+  setShowFilter,
+  showSettings,
+  setShowSettings,
+  appliedSettings,
+  setAppliedSettings,
+}: any) {
   return (
     <>
       <Layout>
-        <Header onFilterClick={() => {}} />
-        <MensaBody />
+        <Header
+          onFilterClick={() => setShowFilter(!showFilter)}
+          onSettingsClick={() => setShowSettings(!showSettings)}
+        />
+        <MensaBody
+          appliedSettings={appliedSettings}
+          showSettings={showSettings}
+          setShowSettings={setShowSettings}
+        />
+        <div>
+          {showSettings && (
+            <Settings
+              appliedSettings={appliedSettings}
+              setAppliedSettings={setAppliedSettings}
+            />
+          )}
+        </div>
       </Layout>
     </>
   );
