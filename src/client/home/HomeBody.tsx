a4951849b481053fb93f9f14b633af5f5df768ef
Lukas Mast <lumast@student.ethz.ch>
2023-12-13 21:55:48 +0100

    Add functionality to display favourite menus which are available today

--- src/client/home/HomeBody.tsx
+++ src/client/home/HomeBody.tsx
@@ -18,8 +18,188 @@ async function fetchMensaStaticInfos() {
   }
 }
 
+/* Makes the first char of every word to uppercase */
+function capitalizeWords(str: string) {
+  return str
+    .split(" ") // Split the string into an array of words
+    .map(
+      (word) => word.charAt(0).toUpperCase() + word.slice(1) // Capitalize the first character of each word
+    )
+    .join(" "); // Join the words back into a single string
+}
+
+function transformMensaTitle(meal: any): string {
+  if (meal.line_name) {
+    if (meal.meal_name) {
+      return (
+        meal.line_name.toUpperCase() + " | " + capitalizeWords(meal.meal_name)
+      );
+    } else {
+      return meal.line_name.toUpperCase();
+    }
+  }
+  return "";
+}
+
+function getPrice(meal: any, priceCategory: string): string {
+  let amount = meal.price_info[priceCategory];
+  return amount.toLocaleString("en-US", {
+    style: "currency",
+    currency: "CHF", // Change the currency code as needed
+    minimumFractionDigits: 2, // Set the minimum number of digits after the decimal point
+  });
+}
+
+function displayMatchedAllergens(
+  allergens: any[],
+  appliedSettings: any
+): string {
+  let allergensString = "";
+  for (const allergen of allergens) {
+    if (markAllergen(allergen, appliedSettings)) {
+      allergensString += allergen + ", ";
+    }
+  }
+  return allergensString.slice(0, -2);
+}
+
+/* Hardcoded logic for handling allergies */
+function markAllergen(allergen: string, appliedSettings: any): boolean {
+  if (allergen.toLowerCase().includes("gluten")) {
+    return appliedSettings.gluten;
+  }
+  if (allergen.toLowerCase().includes("krebstiere")) {
+    return appliedSettings.krebstiere;
+  }
+  if (allergen.toLowerCase().includes("ei")) {
+    return appliedSettings.ei;
+  }
+  if (allergen.toLowerCase().includes("fisch")) {
+    return appliedSettings.fisch;
+  }
+  if (allergen.toLowerCase().includes("erdnüsse")) {
+    return appliedSettings.erdnüsse;
+  }
+  if (allergen.toLowerCase().includes("soja")) {
+    return appliedSettings.soja;
+  }
+  if (
+    allergen.toLowerCase().includes("milch") ||
+    allergen.toLowerCase().includes("laktose")
+  ) {
+    return appliedSettings.milch_laktose;
+  }
+  if (allergen.toLowerCase().includes("schalenfrüchte")) {
+    return appliedSettings.schalenfrüchte;
+  }
+  if (allergen.toLowerCase().includes("sellerie")) {
+    return appliedSettings.sellerie;
+  }
+  if (allergen.toLowerCase().includes("senf")) {
+    return appliedSettings.senf;
+  }
+  if (allergen.toLowerCase().includes("sesam")) {
+    return appliedSettings.sesam;
+  }
+  if (allergen.toLowerCase().includes("sulfite")) {
+    return appliedSettings.sulfite;
+  }
+  if (allergen.toLowerCase().includes("lupinen")) {
+    return appliedSettings.lupinen;
+  }
+  if (allergen.toLowerCase().includes("weichtiere")) {
+    return appliedSettings.weichtiere;
+  }
+  if (allergen.toLowerCase().includes("hartschalenobst")) {
+    return appliedSettings.hartschalenobst;
+  }
+
+  /* Handle case: allergy is not in our list --> Add it to be on the safe side*/
+  console.log(allergen.toUpperCase() + " is not in our list!!!!");
+  return true;
+}
+
+// Fetch menus for today of facility with id menu.facilityID
+async function checkIfMenuIsAvailableToday(
+  menu: any
+): Promise<[boolean, string]> {
+  try {
+    const response = await fetch(`/menus/${menu.facility_id}`);
+    if (!response.ok) {
+      throw new Error("Failed to fetch meals");
+    }
+    const mealsData = await response.json();
+
+    const daysOfWeek = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    const currentDate = new Date();
+    const currentDay = daysOfWeek[currentDate.getDay()] as string;
+
+    // Logic to determine if dinner or lunch
+    if (mealsData[currentDay]) {
+      const meals = mealsData[currentDay];
+
+      if (meals["Lunch"]) {
+        for (const meal of meals["Lunch"]) {
+          if (meal.meal_description === menu.meal_description) {
+            return [true, "Lunch"];
+          }
+        }
+      }
+
+      if (meals["Dinner"]) {
+        for (const meal of meals["Dinner"]) {
+          if (meal.meal_description === menu.meal_description) {
+            return [true, "Dinner"];
+          }
+        }
+      }
+
+      return [false, "Menu not available today"];
+    }
+
+    return [false, "Menu not available today"];
+  } catch (error) {
+    console.error(error);
+    return [false, "Failed to fetch meals"];
+  }
+}
+
+/* Function that returns either "Open" or "Closed", depending on current time */
+function currentlyOpen(mensa: any): boolean {
+  const now = new Date();
+  const nowAsNumber = now.getHours() + now.getMinutes() / 100;
+  if (
+    nowAsNumber < mensa.opening_time_start ||
+    nowAsNumber > mensa.opening_time_end
+  ) {
+    return false;
+  }
+  return true;
+}
+
 interface FavoritesState {
   [key: string]: boolean;
+}
+
+interface Menu {
+  facility_id: number;
+  line_name: string;
+  meal_name: string;
+  meal_description: string;
+  allergens: string[];
+  price_info: {
+    students: number;
+    internal: number;
+    external: number;
+  };
 }
 
 function HomeBody({
@@ -28,8 +208,40 @@ function HomeBody({
   appliedFilters,
   showSettings,
   setShowSettings,
+  appliedSettings,
 }: any) {
   const [mensaInfos, setMensaInfos] = useState<any>([]);
+  const [favouriteMenus, setFavouriteMenus] = useState<Menu[]>([]);
+  const [favouriteMenusToday, setFavouriteMenusToday] = useState<
+    [Menu, string][]
+  >([]);
+
+  // Saves the state of the heart (mensa selected as favorite) for each mensa
+  const [favorites, setFavorites] = useState<FavoritesState>({});
+  const [currentUserId, setCurrentUserId] = useState(-1);
+
+  useEffect(() => {
+    if (currentUserId === -1) {
+      return;
+    }
+    if (favouriteMenus.length === 0) {
+      return;
+    }
+    // For each menu in favouriteMenus, check if it is available today
+    async function checkIfMenusAreAvailableToday() {
+      const favouriteMenusToday: [Menu, string][] = [];
+      for (const menu of favouriteMenus) {
+        const [isAvailableToday, daytime] =
+          await checkIfMenuIsAvailableToday(menu);
+        if (isAvailableToday) {
+          favouriteMenusToday.push([menu, daytime]);
+        }
+      }
+      setFavouriteMenusToday(favouriteMenusToday);
+    }
+
+    checkIfMenusAreAvailableToday();
+  }, [favouriteMenus]);
 
   useEffect(() => {
     async function fetchMeals() {
@@ -42,9 +254,6 @@ function HomeBody({
     }
     fetchMeals();
   }, []);
-  // Saves the state of the heart (mensa selected as favorite) for each mensa
-  const [favorites, setFavorites] = useState<FavoritesState>({});
-  const [currentUserId, setCurrentUserId] = useState(-1);
 
   useEffect(() => {
     async function fetchUserID() {
@@ -65,6 +274,26 @@ function HomeBody({
     }
     fetchUserID();
   }, []);
+
+  useEffect(() => {
+    async function fetchFavouriteMenus() {
+      try {
+        if (currentUserId === -1) {
+          return;
+        }
+        const response = await fetch(`/serve-favourite-menus/${currentUserId}`);
+        if (!response.ok) {
+          throw new Error("Failed to fetch favorite meals");
+        }
+        const favouriteMenus = await response.json();
+        setFavouriteMenus(favouriteMenus.favouriteMenus);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchFavouriteMenus();
+  }, [currentUserId]);
 
   /* Filter functionality */
   const noFiltersApplied = Object.values(appliedFilters).every(
@@ -117,6 +346,19 @@ function HomeBody({
     navigate("/" + mensaName);
   };
 
+  // function to translate facility_id to mensa name
+  function translateFacilityIDtoMensaName(
+    facility_id: number,
+    daytime: string
+  ): string {
+    const foundMensa = mensaInfos.find(
+      (mensa: any) =>
+        mensa.facility_id === facility_id.toString() &&
+        mensa.daytime === daytime
+    );
+    return foundMensa ? foundMensa.name_display : "Mensa not found";
+  }
+
   return (
     <>
       <main
@@ -135,22 +377,55 @@ function HomeBody({
         )}
         {currentUserId !== -1 && (
           <>
-            <div className="favourite-menus-container">
+            <div className="todays-favourite-menus-container">
               {currentUserId !== -1 && (
                 <>
                   <h2>Your favourite menus today</h2>
                   <FaEdit
                     size={30}
                     onClick={() => navigate("/favourite-menus")}
+                    className="edit-favourites-button"
                   />
+                  <div className="menu-container">
+                    {/* Create for each menu a component */}
+                    {favouriteMenusToday.map(([meal, daytime], index) => (
+                      <div key={index} className="menu-component">
+                        <h3 className="mensa-name">
+                          Available at{" "}
+                          {translateFacilityIDtoMensaName(
+                            meal.facility_id,
+                            daytime
+                          )}
+                        </h3>
+                        <h3 className="menu-title-and-price">
+                          <p className="menu-title">
+                            {transformMensaTitle(meal)}
+                          </p>
+                          <p className="menu-price">
+                            {getPrice(meal, appliedSettings.price_class)}
+                          </p>
+                        </h3>
+                        {/* change later: priceCategory should be retrieved from user preference */}
+                        <div className="ingredients-component">
+                          {meal.meal_description}
+                        </div>
+                        <div className="matched-allergens-component">
+                          {meal.allergens && meal.allergens.length === 0
+                            ? "No allergy information available"
+                            : displayMatchedAllergens(
+                                meal.allergens,
+                                appliedSettings
+                              )}
+                        </div>
+                      </div>
+                    ))}
+                    {favouriteMenus.length === 0 && (
+                      <div className="banner-no-menus-available">
+                        No favourite menus today!
+                      </div>
+                    )}
+                  </div>
                 </>
-              )}
-            </div>
-            <div className="todays-favourite-menus-container">
-              {currentUserId !== -1 ? (
-                <div>Welcome, User ID: {currentUserId}</div>
-              ) : (
-                <div>No user logged in...</div>
               )}
             </div>
           </>
@@ -200,16 +475,3 @@ function HomeBody({
 }
 
 export default HomeBody;
-
-/* Function that returns either "Open" or "Closed", depending on current time */
-function currentlyOpen(mensa: any): boolean {
-  const now = new Date();
-  const nowAsNumber = now.getHours() + now.getMinutes() / 100;
-  if (
-    nowAsNumber < mensa.opening_time_start ||
-    nowAsNumber > mensa.opening_time_end
-  ) {
-    return false;
-  }
-  return true;
-}
