7753e32d04c9a5a0296c9326426c099eef34719e
Lukas Mast <lumast@student.ethz.ch>
2023-12-16 23:38:23 +0100

    Remove unused imports

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-// import "@fortawesome/fontawesome-free/css/all.css";
-// import "@picocss/pico/css/pico.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
